110716dca3535c8f152da7c9f360d0166971d3e4
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2024-09-12 16:35:48 -0500

    Finalized the NavBar (I think)

--- Fall2024-Assignment1-jtadams5/wwwroot/js/site.js
+++ Fall2024-Assignment1-jtadams5/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getAbsoluteHeight(ele) {
+    el = (typeof el === 'string') ? document.querySelector(el) : el;
+    var styles = window.getComputedStyle(el);
+    var margin = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom']);
+    return Math.ceil(el.offsetHeight + margin);
+}
